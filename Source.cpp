acdd5411cdd3faf0ac1385004b459ff511dd83a8
Amitesh Nagavalli <amiteshnagavalli@yahoo.com>
2023-10-01 20:58:49 -0700

    refactored operand functions; added a lot more instructions

--- Source.cpp
+++ Source.cpp
@@ -305,62 +305,99 @@ string EAClookup(int MOD, int R_M, int W, int segOverride)
 //}
 
 // test cases
-//000000010101000111101111 = ADD     DX,[BX+DI+EF]		
+//000000010101000111101111 = ADD     [BX+DI+EF],DX		
 //000001011110111110101011 = ADD     AX,[EFAB]
-//000010000101010011101111 = OR      DL,[SI+EF]
+//000010000101010011101111 = OR      [SI+EF],DL
+
+string regMemOperands(int W, int D, int segOverride)
+{
+	string regStr, EACStr;
+
+	int mod = readNumBits(2);
+	int reg = readNumBits(3);
+	int r_m = readNumBits(3);
+
+	EACStr = EAClookup(mod, r_m, W, segOverride);
+	regStr = registerLookup(reg, W);
+
+	if (D) {	// D == 1; reg first
+		return regStr + "," + EACStr;
+	}
+	else {		// D == 0; EAC first
+		return EACStr + "," + regStr;
+	}
+}
+
+string immedOperands(int W)
+{
+	if (W) {	// W == 1; 16-bit
+		int data = readNumBits(16);
+		return "AX,[" + intToHexStr(data, 16) + "]";
+	}
+	else {		// W == 0; 8-bit
+		int data = readNumBits(8);
+		return "AL,[" + intToHexStr(data, 8) + "]";
+	}
+}
 
 string decodeInstr(int byte, int segOverride)
 {
 	switch (byte) {
 		case 0x00:	// ADD	REG8/MEM8,REG8
 		{
-			int mod = readNumBits(2);
+			/*int mod = readNumBits(2);
 			int reg = readNumBits(3);
-			int r_m = readNumBits(3);
+			int r_m = readNumBits(3);*/
 
-			string output = "ADD\t" + registerLookup(reg, 0) + "," + EAClookup(mod, r_m, 0, segOverride);
-			return output;
+			return "ADD\t" + regMemOperands(0, 0, segOverride);
+			//string output = "ADD\t" + EAClookup(mod, r_m, 0, segOverride) + "," + registerLookup(reg, 0);
+			//return output;
 		}
 		case 0x01:	// ADD	REG16/MEM16,REG16
 		{
-			int mod = readNumBits(2);
+			/*int mod = readNumBits(2);
 			int reg = readNumBits(3);
-			int r_m = readNumBits(3);
-
-			string output = "ADD\t" + registerLookup(reg, 1) + "," + EAClookup(mod, r_m, 1, segOverride);
-			return output;
+			int r_m = readNumBits(3);*/
+			
+			return "ADD\t" + regMemOperands(1, 0, segOverride);
+			/*string output = "ADD\t" + EAClookup(mod, r_m, 1, segOverride) + "," + registerLookup(reg, 1);
+			return output;*/
 		}
 		case 0x02:	// ADD	REG8,REG8/MEM8
 		{
-			int mod = readNumBits(2);
+			/*int mod = readNumBits(2);
 			int reg = readNumBits(3);
-			int r_m = readNumBits(3);
+			int r_m = readNumBits(3);*/
 
-			string output = "ADD\t" + EAClookup(mod, r_m, 0, segOverride) + "," + registerLookup(reg, 0);
-			return output;
+			return "ADD\t" + regMemOperands(0, 1, segOverride);
+			/*string output = "ADD\t" + registerLookup(reg, 0) + "," + EAClookup(mod, r_m, 0, segOverride);
+			return output;*/
 		}
 		case 0x03:	// ADD	REG16,REG16/MEM16
 		{
-			int mod = readNumBits(2);
+			/*int mod = readNumBits(2);
 			int reg = readNumBits(3);
-			int r_m = readNumBits(3);
+			int r_m = readNumBits(3);*/
 
-			string output = "ADD\t" + EAClookup(mod, r_m, 1, segOverride) + "," + registerLookup(reg, 1);
-			return output;
+			return "ADD\t" + regMemOperands(1, 1, segOverride);
+			/*string output = "ADD\t" + registerLookup(reg, 1) + "," + EAClookup(mod, r_m, 1, segOverride);
+			return output;*/
 		}
 		case 0x04:	// ADD	AL,IMMED8
 		{
-			int data = readNumBits(8);
+			return "ADD\t" + immedOperands(0);
+			/*int data = readNumBits(8);
 
 			string output = "ADD\tAL,[" + intToHexStr(data, 8) + "]";
-			return output;
+			return output;*/
 		}
 		case 0x05:	// ADD	AX,IMMED16
 		{
-			int data = readNumBits(16);
+			return "ADD\t" + immedOperands(16);
+			/*int data = readNumBits(16);
 
 			string output = "ADD\tAX,[" + intToHexStr(data, 16) + "]";
-			return output;
+			return output;*/
 		}
 		case 0x06:	// PUSH	ES
 		{
@@ -372,53 +409,59 @@ string decodeInstr(int byte, int segOverride)
 		}
 		case 0x08:	// OR	REG8/MEM8,REG8
 		{
-			int mod = readNumBits(2);
+			/*int mod = readNumBits(2);
 			int reg = readNumBits(3);
-			int r_m = readNumBits(3);
+			int r_m = readNumBits(3);*/
 
-			string output = "OR\t" + registerLookup(reg, 0) + "," + EAClookup(mod, r_m, 0, segOverride);
-			return output;
+			return "OR\t" + regMemOperands(0, 0, segOverride);
+			/*string output = "OR\t" + EAClookup(mod, r_m, 0, segOverride) + "," + registerLookup(reg, 0);
+			return output;*/
 		}
 		case 0x09:	// OR	REG16/MEM16,REG16
 		{
-			int mod = readNumBits(2);
+			/*int mod = readNumBits(2);
 			int reg = readNumBits(3);
-			int r_m = readNumBits(3);
+			int r_m = readNumBits(3);*/
 
-			string output = "OR\t" + registerLookup(reg, 1) + "," + EAClookup(mod, r_m, 1, segOverride);
-			return output;
+			return "OR\t" + regMemOperands(1, 0, segOverride);
+			/*string output = "OR\t" + EAClookup(mod, r_m, 1, segOverride) + "," + registerLookup(reg, 1);
+			return output;*/
 		}
 		case 0x0A:	// OR	REG8,REG8/MEM8
 		{
-			int mod = readNumBits(2);
+			/*int mod = readNumBits(2);
 			int reg = readNumBits(3);
-			int r_m = readNumBits(3);
+			int r_m = readNumBits(3);*/
 
-			string output = "OR\t" + EAClookup(mod, r_m, 0, segOverride) + "," + registerLookup(reg, 0);
-			return output;
+			return "OR\t" + regMemOperands(0, 1, segOverride);
+			/*string output = "OR\t" + registerLookup(reg, 0) + "," + EAClookup(mod, r_m, 0, segOverride);
+			return output;*/
 		}
 		case 0x0B:	// OR	REG16,REG16/MEM16
 		{
-			int mod = readNumBits(2);
+			/*int mod = readNumBits(2);
 			int reg = readNumBits(3);
-			int r_m = readNumBits(3);
+			int r_m = readNumBits(3);*/
 
-			string output = "OR\t" + EAClookup(mod, r_m, 1, segOverride) + "," + registerLookup(reg, 1);
-			return output;
+			return "OR\t" + regMemOperands(1, 1, segOverride);
+			/*string output = "OR\t" + registerLookup(reg, 1) + "," + EAClookup(mod, r_m, 1, segOverride);
+			return output;*/
 		}
 		case 0x0C:	// OR	AL,IMMED8
 		{
-			int data = readNumBits(8);
+			return "OR\t" + immedOperands(0);
+			/*int data = readNumBits(8);
 
 			string output = "OR\tAL,[" + intToHexStr(data, 8) + "]";
-			return output;
+			return output;*/
 		}
 		case 0x0D:	// OR	AX,IMMED16
 		{
-			int data = readNumBits(16);
+			return "OR\t" + immedOperands(1);
+			/*int data = readNumBits(16);
 
 			string output = "ADD\tAX,[" + intToHexStr(data, 16) + "]";
-			return output;
+			return output;*/
 		}
 		case 0x0E:	// PUSH	CS
 		{
@@ -428,14 +471,126 @@ string decodeInstr(int byte, int segOverride)
 		{
 			return "NOT USED";
 		}
-
-
-		// testing segment overrides
-		case 0x26:
+		case 0x10:	// ADC	REG8/MEM8,REG8
+		{
+			return "ADC\t" + regMemOperands(0, 0, segOverride);
+		}
+		case 0x11:	// ADC	REG16/MEM16,REG16
+		{
+			return "ADC\t" + regMemOperands(1, 0, segOverride);
+		}
+		case 0x12:	// ADC	REG8,REG8/MEM8
+		{
+			return "ADC\t" + regMemOperands(0, 1, segOverride);
+		}
+		case 0x13:	// ADC REG16,REG16/MEM16
+		{
+			return "ADC\t" + regMemOperands(1, 1, segOverride);
+		}
+		case 0x14:	// ADC	AL,IMMED8
+		{
+			return "ADC\t" + immedOperands(0);
+		}
+		case 0x15:	// ADC	AL,IMMED16
+		{
+			return "ADC\t" + immedOperands(1);
+		}
+		case 0x16:	// PUSH	SS
+		{
+			return "PUSH\tSS";
+		}
+		case 0x17:	// POP SS
+		{
+			return "POP\tSS";
+		}
+		case 0x18:	// SBB	REG8/MEM8,REG8
+		{
+			return "SBB\t" + regMemOperands(0, 0, segOverride);
+		}
+		case 0x19:	// SBB	REG16/MEM16,REG16
+		{
+			return "SBB\t" + regMemOperands(1, 0, segOverride);
+		}
+		case 0x1A:	// SBB	REG8,REG8/MEM8
+		{
+			return "SBB\t" + regMemOperands(0, 1, segOverride);
+		}
+		case 0x1B:	// SBB	REG16,REG16/MEM16
+		{
+			return "SBB\t" + regMemOperands(1, 1, segOverride);
+		}
+		case 0x1C:	// SBB	AL,IMMED8
+		{
+			return "SBB\t" + immedOperands(0);
+		}
+		case 0x1D:	// SBB	AX,IMMED16
+		{
+			return "SBB\t" + immedOperands(1);
+		}
+		case 0x1E:	// PUSH	DS
+		{
+			return "PUSH\tDS";
+		}
+		case 0x1F:	// POP	DS
+		{
+			return "POP\tDS";
+		}
+		case 0x20:	// AND	REG8/MEM8,REG8
+		{
+			return "AND\t" + regMemOperands(0, 0, segOverride);
+		}
+		case 0x21:	// AND	REG16/MEM16,REG16
+		{
+			return "AND\t" + regMemOperands(1, 0, segOverride);
+		}
+		case 0x22:	// AND	REG8,REG8/MEM8
+		{
+			return "AND\t" + regMemOperands(0, 1, segOverride);
+		}
+		case 0x23:	// AND	REG16,REG16/MEM16
+		{
+			return "AND\t" + regMemOperands(1, 1, segOverride);
+		}
+		case 0x24:	// AND	AL,IMMED8
+		{
+			return "AND\t" + immedOperands(0);
+		}
+		case 0x25:	// AND	AX,IMMED16
+		{
+			return "AND\t" + immedOperands(1);
+		}
+		case 0x26:	// ES: segment override prefix
 		{
 			return decodeInstr(readNumBits(8), ES);
 		}
-
+		case 0x27:	// DAA
+		{
+			return "DAA";
+		}
+		case 0x28:	// SUB	REG8/MEM8,REG8
+		{
+			return "SUB\t" + regMemOperands(0, 0, segOverride);
+		}
+		case 0x29:	// SUB	REG16/MEM16,REG16
+		{
+			return "SUB\t" + regMemOperands(1, 0, segOverride);
+		}
+		case 0x2A:	// SUB	REG8,REG8/MEM8
+		{
+			return "SUB\t" + regMemOperands(0, 1, segOverride);
+		}
+		case 0x2B:	// SUB	REG16,REG16/MEM16
+		{
+			return "SUB\t" + regMemOperands(1, 1, segOverride);
+		}
+		case 0x2C:	// SUB	AL,IMMED8
+		{
+			return "SUB\t" + immedOperands(0);
+		}
+		case 0x2D:	// SUB	AX,IMMED16
+		{
+			return "SUB\t" + immedOperands(1);
+		}
 		default:
 			return "COULDN'T DECODE INSTRUCTION";
 			break;
